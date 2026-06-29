e37c88c25fc550528e3be9e498f71d95905448c9
sbories <soeurgrimm@gmail.com>
2026-06-29 18:40:20 -0400

    add input and output structures of 0.1M

--- supp_files/method_script/5-postprocessing/get_last_frames.sh
+++ supp_files/method_script/5-postprocessing/get_last_frames.sh
@@ -5,7 +5,7 @@ aafile=( \
   "SCF" "SCY" "SCW" "SCP" "GLYD" \
   "SCHE" "SCHD" "SCDN" "SCEN" "SCKN" "SCRN" \
   "SCHP" "SCD" "SCE" "SCCM" "SCYM" "SCK" "SCR" \
-  "SCRN-1" "SCW-1" \
+  "SCRN-1" "SCW-1" "SCF-1" "SCY-1" "SCL-1" "SCV-1" "SCI-1" "SCM-1" \
   "NONE" \
 )
 
@@ -14,14 +14,14 @@ traj=(1 2 3)
 
 for aa in "${aafile[@]}"
 do
-  mkdir ${aa,,}
+  mkdir ../../output_systems/${aa,,}
   for t in "${traj[@]}"
   do
     cp ../../results/POPC-aa/POPC-$aa/charmm-gui/namd/out$t/section1000.dcd ./trajectory$t.dcd
-    cp -r trajectory$t.dcd traj.dcd
-    cp ../../results/POPC-aa/POPC-${aa,,}/charmm-gui/namd/step5_input.psf ./input.psf
+    cp trajectory$t.dcd traj.dcd
+    cp ../../results/POPC-aa/POPC-$aa/charmm-gui/namd/step5_input.psf ./input.psf
     vmd -dispdev text -e get_frame.tcl
-    mv last_frame.pdb ../../../output_systems/${aa,,}/end_traj$t.pdb
+    mv last_frame.pdb ../../output_systems/${aa,,}/end_traj$t.pdb
   done
   rm trajectory* input* traj.dcd
 done
